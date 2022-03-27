b869c8f7cf136c47a570f74b09f2ad679d367bab
mitay-walle <mitay-walle91@mail.ru>
2022-03-27 15:04:52 +0300

    Search, Expanded, EditorPrefs

--- Editor/ContextMenuWindow.cs
+++ Editor/ContextMenuWindow.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Editor
 {
     public class ContextMenuWindow : EditorWindow
     {
+        private const string TYPE_NAME = nameof(ContextMenuWindow);
         private const string TAB = "    ";
 
+        [Serializable]
         private class NestedString
         {
             public string Value;
             public string Name;
+            public string ValueLower;
+            public bool IsExpanded;
             public int Depth;
             public NestedString Parent;
             public List<NestedString> Children = new List<NestedString>();
@@ -23,10 +29,15 @@ namespace Editor
             }
         }
 
+        [SerializeField] private string _filter;
+        private string _filterLower;
         [SerializeField] private Vector2Int ButtonSize = new Vector2Int(200, 20);
         [SerializeField] private int Indent = 15;
+        [SerializeField] private bool ShowLines;
+        [SerializeField] private bool ShowLabels;
+        [SerializeField] private bool UseExpandable;
 
-        private NestedString Data = new NestedString("Assets/Create");
+        [SerializeField] private NestedString Data = new NestedString("Assets/Create") {IsExpanded = true};
         private Vector2 scroll;
 
         [MenuItem("Window/Create Context Menu Window")]
@@ -38,8 +49,62 @@ namespace Editor
 
         void OnEnable()
         {
-            titleContent = new GUIContent("Create Context Menu");
+            LoadSettings();
 
+            titleContent = new GUIContent("Create Context Menu");
+            CollectBuiltInItems();
+        }
+
+        private void LoadSettings()
+        {
+            string key = $"{TYPE_NAME}_Indent";
+            if (EditorPrefs.HasKey(key)) Indent = EditorPrefs.GetInt(key, Indent);
+
+            key = $"{TYPE_NAME}_ButtonSize.x";
+            if (EditorPrefs.HasKey(key)) ButtonSize.x = EditorPrefs.GetInt(key, ButtonSize.x);
+            key = $"{TYPE_NAME}_ButtonSize.y";
+            if (EditorPrefs.HasKey(key)) ButtonSize.y = EditorPrefs.GetInt(key, ButtonSize.y);
+
+            key = $"{TYPE_NAME}_ShowLines";
+            if (EditorPrefs.HasKey(key)) ShowLines = EditorPrefs.GetBool(key, ShowLines);
+            key = $"{TYPE_NAME}_ShowLabels";
+            if (EditorPrefs.HasKey(key)) ShowLabels = EditorPrefs.GetBool(key, ShowLabels);
+            key = $"{TYPE_NAME}_UseExpandable";
+            if (EditorPrefs.HasKey(key)) UseExpandable = EditorPrefs.GetBool(key, UseExpandable);
+        }
+
+        private void DrawSettings()
+        { 
+            DrawLine();
+
+            EditorGUI.BeginChangeCheck();
+            Indent = EditorGUILayout.IntField("Indent", Indent);
+            if (EditorGUI.EndChangeCheck()) EditorPrefs.SetInt($"{TYPE_NAME}_Indent", Indent);
+            EditorGUI.BeginChangeCheck();
+            ButtonSize = EditorGUILayout.Vector2IntField("Button Height", ButtonSize);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                //Debug.Log($"set {ButtonSize.x}");
+                EditorPrefs.SetInt($"{TYPE_NAME}_ButtonSize.x", ButtonSize.x);
+                EditorPrefs.SetInt($"{TYPE_NAME}_ButtonSize.y", ButtonSize.y);
+            }
+
+            EditorGUI.BeginChangeCheck();
+            ShowLines = EditorGUILayout.Toggle("Lines", ShowLines);
+            if (EditorGUI.EndChangeCheck()) EditorPrefs.SetBool($"{TYPE_NAME}_ShowLines", ShowLines);
+
+            EditorGUI.BeginChangeCheck();
+            ShowLabels = EditorGUILayout.Toggle("Labels", ShowLabels);
+            if (EditorGUI.EndChangeCheck()) EditorPrefs.SetBool($"{TYPE_NAME}_ShowLabels", ShowLabels);
+
+            EditorGUI.BeginChangeCheck();
+            UseExpandable = EditorGUILayout.Toggle("Expandable", UseExpandable);
+            if (EditorGUI.EndChangeCheck()) EditorPrefs.SetBool($"{TYPE_NAME}_UseExpandable", UseExpandable);
+        }
+
+        void CollectBuiltInItems()
+        {
             string stringData = EditorGUIUtility.SerializeMainMenuToString();
 
 
@@ -87,6 +152,8 @@ namespace Editor
                 if (newDepth == previous.Depth)
                 {
                     current.Value = lastParent.Value + '/' + current.Value;
+                    current.ValueLower = current.Value.ToLowerInvariant();
+
                     current.Parent = lastParent;
                     lastParent.Children.Add(current);
 
@@ -101,18 +168,21 @@ namespace Editor
 
                         while (current.Depth - temp.Depth != 1)
                         {
-                            temp = temp.Parent;
+                            Debug.Log($"temp '{temp.Name}' | temp {temp.Depth} | current {current.Depth}");
 
-                            //Debug.Log($"temp '{temp.Name}' | temp {temp.Depth} | current {current.Depth}");
+                            if (temp.Parent == null) break;
+
+                            temp = temp.Parent;
                         }
 
                         lastParent = temp;
 
-                        //Debug.Log($"new last parent 2 '{temp.Name}'");
+                        Debug.Log($"new last parent 2 '{temp.Name}'");
                     }
                     else
                     {
                         current.Value = previous.Value + '/' + current.Value;
+                        current.ValueLower = current.Value.ToLowerInvariant();
                         current.Parent = previous;
                         previous.Children.Add(current);
                         lastParent = previous;
@@ -129,34 +199,73 @@ namespace Editor
 
         void OnGUI()
         {
+            DrawSearch();
             scroll = GUILayout.BeginScrollView(scroll);
             int index = 0;
-            Draw(Data,ref index);
+            Draw(Data, ref index);
             GUILayout.EndScrollView();
-            DrawLine();
-            Indent = EditorGUILayout.IntField("Indent", Indent);
-            ButtonSize = EditorGUILayout.Vector2IntField("Button Height", ButtonSize);
+            DrawSettings();
         }
 
-        private void Draw(NestedString target,ref int index)
+        private void DrawSearch()
         {
+            EditorGUI.BeginChangeCheck();
+
+            string newFilter = EditorGUILayout.TextField("Search", _filter);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                _filter = newFilter;
+                _filterLower = _filter.ToLowerInvariant();
+            }
+        }
+
+        private void Draw(NestedString target, ref int index)
+        {
+            if (target.Children.Count ==0 && !string.IsNullOrEmpty(_filterLower) && !target.ValueLower.Contains(_filterLower)) return;
+                
             GUILayout.BeginHorizontal(GUILayout.MaxWidth(position.width));
-            GUILayout.Space(target.Depth * Indent);
-   
+            GUILayout.Space((target.Depth - 1) * Indent);
+
             if (target.Children.Count > 0)
             {
-                GUILayout.Label(target.Name);
-                DrawLine();
+                if (UseExpandable)
+                {
+                    if (GUILayout.Button($"{(target.IsExpanded?'-':'+')} {target.Name}","label", GUILayout.Height(ButtonSize.y), GUILayout.Width(ButtonSize.x)))
+                    {
+                        target.IsExpanded = !target.IsExpanded;
+                    }
+                }
+                else
+                {
+                    GUILayout.Label(target.Name);
+                }
+
+                if (ShowLines)
+                {
+                    DrawLine();
+                }
             }
             else
             {
-                if (GUILayout.Button(target.Name, GUILayout.Height(ButtonSize.y), GUILayout.Width(ButtonSize.x)))
+                if (ShowLabels)
                 {
-                    Selection.activeObject = GetSelectedPathOrFallback();
-                    EditorApplication.ExecuteMenuItem(target.Value);
-                }
+                    if (GUILayout.Button(target.Name, GUILayout.Height(ButtonSize.y), GUILayout.Width(ButtonSize.x)))
+                    {
+                        Selection.activeObject = GetSelectedPathOrFallback();
+                        EditorApplication.ExecuteMenuItem(target.Value);
+                    }
 
-                GUILayout.Label(target.Value);
+                    GUILayout.Label(target.Value);
+                }
+                else
+                {
+                    if (GUILayout.Button(target.Name, GUILayout.Height(ButtonSize.y)))
+                    {
+                        Selection.activeObject = GetSelectedPathOrFallback();
+                        EditorApplication.ExecuteMenuItem(target.Value);
+                    } 
+                }
             }
 
             GUI.backgroundColor = Color.white;
@@ -164,9 +273,12 @@ namespace Editor
             GUILayout.EndHorizontal();
 
             index++;
+
+            if (UseExpandable && !target.IsExpanded && string.IsNullOrEmpty(_filter)) return;
+
             foreach (NestedString child in target.Children)
             {
-                Draw(child,ref index);
+                Draw(child, ref index);
             }
         }
 
